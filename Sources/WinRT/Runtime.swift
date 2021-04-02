ced3c83a58520dbc644241b45f3b01cdb4515425
Saleem Abdulrasool <compnerd@compnerd.org>
2021-04-01 23:22:27 -0700

    ABI: add IDispatcherQueueControllerStatics
    Add bindings for `IDispatcherQueueControllerStatics`.

--- Sources/WinRT/Runtime.swift
+++ Sources/WinRT/Runtime.swift
@@ -4,6 +4,14 @@
 import CWinRT
 
 @_transparent
+public var RuntimeClass_Windows_System_DispatcherQueueController: String {
+  return withUnsafeBytes(of: CWinRT.RuntimeClass_Windows_System_DispatcherQueueController) {
+    String(decodingCString: $0.baseAddress!.assumingMemoryBound(to: UTF16.CodeUnit.self),
+           as: UTF16.self)
+  }
+}
+
+@_transparent
 public var RuntimeClass_Windows_System_Profile_SystemIdentification: String {
   return withUnsafeBytes(of: CWinRT.RuntimeClass_Windows_System_Profile_SystemIdentification) {
     String(decodingCString: $0.baseAddress!.assumingMemoryBound(to: UTF16.CodeUnit.self),
